0246c3505b9c74c9907acb864d646fa70e3054e5
Звягинцев Михаил Евгеньевич <mishacos1110@gmail.com>
2022-05-20 15:20:03 +0700

    Edit thirdparty

--- thirdparty/Ctest.h
+++ thirdparty/Ctest.h
@@ -1,18 +1,3 @@
-/* Copyright 2011-2022 Bas van den Berg
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 #ifndef CTEST_H
 #define CTEST_H
 
@@ -75,7 +60,7 @@ struct ctest {
     unsigned int magic;
 };
 
-#define CTEST_IMPL_NAME(name) ctest_##name
+#define CTEST_IMPL_NAME(name) Ctest_##name
 #define CTEST_IMPL_FNAME(sname, tname) CTEST_IMPL_NAME(sname##_##tname##_run)
 #define CTEST_IMPL_TNAME(sname, tname) CTEST_IMPL_NAME(sname##_##tname)
 #define CTEST_IMPL_DATA_SNAME(sname) CTEST_IMPL_NAME(sname##_data)
@@ -587,4 +572,3 @@ __attribute__((no_sanitize_address)) int ctest_main(int argc, const char *argv[]
 #endif
 
 #endif
-
